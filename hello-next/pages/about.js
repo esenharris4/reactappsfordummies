d6a43db3ad2827123e919b78d311a0a1b643ed67
Esen Harris <esharris@bu.edu>
2018-08-13 11:00:13 -0400

    some buttons and some jokes

---
+++ hello-next/pages/about.js
@@ -0,0 +1,6 @@
+ export default () => (
+	<div>
+	  <p> This is the about page that doesn't have anything to show for it. The question of the day is: is it better to use different frameworks to make your job easier, or to become an export at the language you are trying to use?
+	  </p>
+	</div>
+)
--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -1,7 +1,14 @@
-	const Index = () => (
-		<div>
-		  <p> Hello Next.js</p>
-		</div>
-	)
+// This is the Link API
+
+import Link from 'next/link'
+
+const Index = () => (
+<div>
+	<Link href ="/about">
+		<button> Lets Go To This About Pages Explaining My Day </button>
+	</Link>
+ 	<p> Hello Next.js</p>
+</div>
+)
 	
 export default Index
